4c7411141d02ef730a22389a0313cefc8f6a796b
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-12-14 20:47:07 -0300

    aula 244 - interceptando Requisições

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -11,6 +11,11 @@ Vue.use({
         })
 
 
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            return config
+        })
+
         //,
         // Vue.prototype.$teste = axios.create({
         //     baseURL: 'https://curso-vue-75acf-default-rtdb.firebaseio.com/'
